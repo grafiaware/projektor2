fce2950bfbf32246a36a2b11e9c94398dc754dba
pes2704 <pes2704@grafia.cz>
2024-01-31 17:14:03 +0100

    config - produkční db na xenon

--- Config/AppContext.php
+++ Config/AppContext.php
@@ -32,7 +32,7 @@ abstract class Config_AppContext
                 if(!isset(self::$db['projektor']) OR !isset(self::$db['projektor'])) {
                     if (self::isRunningOnProductionMachine()) {
 //                        $dbh = new Framework_Database_HandlerSqlMysql("projektor_2", "root", "spravce", "neon");
-                        $dbh = new Framework_Database_HandlerSqlMysql("projektor_2_cjc", "root", "spravce", "neon");
+                        $dbh = new Framework_Database_HandlerSqlMysql("projektor_2_cjc", "root", "spravce", "xenon");
                     } else {
 //                        $dbh = new Framework_Database_HandlerSqlMysql("projektor_2", "root", "spravce", "localhost");
                         $dbh = new Framework_Database_HandlerSqlMysql("projektor_2_cjc", "root", "spravce", "localhost");
